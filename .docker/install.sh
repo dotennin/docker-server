87e3237920074e78aa3eddb0365828ffb3d0295d
nochi0105 <Wh314751>
2017-06-07 17:23:54 +0900

    add network checker

--- .docker/install.sh
+++ .docker/install.sh
@@ -7,6 +7,11 @@ else
 	sudo sh -c "echo '127.0.0.1   $1' >> /etc/hosts";
 fi
 
+docker network ls | grep nginx-net &> /dev/null
+if [ $? == 1 ]; then
+	docker network create nginx-net
+fi
+
 cd $PWD/.docker/ && \
 	export SERVER_NAME=$1 && \
 	export NGINX_ROOT=$2 && \
